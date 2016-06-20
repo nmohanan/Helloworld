c3166a216c6791a728b0ceaaa10041227aa75b49
suyati <nmohanan>
2016-06-20 15:32:05 +0530

    adding new repo to git magento2 hello wolrd

---
+++ Helloworld/Suyati/HelloWorld/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace Suyati\HelloWorld\Block;
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{    
+    protected function _prepareLayout()
+    {
+    
+    }
+}
